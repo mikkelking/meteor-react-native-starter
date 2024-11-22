3dba440d7de22961588da3e71d1df92bdc5bec2a
Mike King <mikkelking@hotmail.com>
2024-11-23 00:27:32 +1100

    Having trouble

--- backend/api/main.js
+++ backend/api/main.js
@@ -1,15 +1,9 @@
-import { Migrations } from 'meteor/quave:migrations';
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from "meteor/meteor";
 
-import './db/migrations';
-import './tasks/tasks.publications';
-import './tasks/tasks.methods';
+import "./tasks/tasks.publications";
+import "./tasks/tasks.methods";
 
 /**
  * This is the server-side entry point
  */
-Meteor.startup(() => {
-  Migrations.migrateTo('latest').catch((e) =>
-    console.error('Error running migrations', e)
-  );
-});
+Meteor.startup(() => {});
